282bf80e1030a2dc69fd84d762b69b73cb1a4d47
jdamilaspmys <jdamilaspmys@gmail.com>
2022-07-07 07:27:55 +0530

    Add pagination, sortby and orderby to list movies api

---
+++ server/constant/defaultValues.js
@@ -0,0 +1,13 @@
+const DEFAULT_PAGINATION = {
+  LIMIT: 10,
+  PAGE: 1,
+};
+
+const DEFAULT_SORT_BY_ORDER_BY = {
+  MOVIES: { SORY_BY: "Title", ORDER_BY: "asc" },
+};
+
+module.exports = {
+  DEFAULT_PAGINATION,
+  DEFAULT_SORT_BY_ORDER_BY,
+};
--- server/routes/movies.js
+++ server/routes/movies.js
@@ -2,17 +2,28 @@ var express = require("express");
 var router = express.Router();
 
 const { SAMPLE_MOVIES } = require("../constant/dummy");
+const {
+  DEFAULT_PAGINATION,
+  DEFAULT_SORT_BY_ORDER_BY,
+} = require("../constant/defaultValues");
 
 /* GET movies listing. */
 router.get("/", function (req, res, next) {
-  res.status(200).json(SAMPLE_MOVIES);
+  const {
+    page = DEFAULT_PAGINATION.PAGE,
+    limit = DEFAULT_PAGINATION.LIMIT,
+    sortBy = DEFAULT_SORT_BY_ORDER_BY.MOVIES.SORY_BY,
+    orderBy = DEFAULT_SORT_BY_ORDER_BY.MOVIES.ORDER_BY,
+  } = req.query || {};
+  res
+    .status(200)
+    .json({ data: SAMPLE_MOVIES, meta: { page, limit, sortBy, orderBy } });
 });
 
 /* GET movie by Id */
 
 router.get("/:id", function (req, res, next) {
   const id = req.params.id;
-  console.log(id);
   const movie = SAMPLE_MOVIES.find((movie) => movie.id === id);
   if (movie) {
     res.status(200).json(movie);
