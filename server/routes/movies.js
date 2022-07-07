5861157d2f908bfaf0ec9dbaa8d16282dd37b21e
jdamilaspmys <jdamilaspmys@gmail.com>
2022-07-07 08:37:44 +0530

    add try/catch for movies routes

--- server/routes/movies.js
+++ server/routes/movies.js
@@ -10,37 +10,45 @@ const Movie = require("../models/Movie");
 
 /* GET movies listing. */
 router.get("/", async (req, res, next) => {
-  const {
-    page = DEFAULT_PAGINATION.PAGE,
-    limit = DEFAULT_PAGINATION.LIMIT,
-    sortBy = DEFAULT_SORT_BY_ORDER_BY.MOVIES.SORY_BY,
-    orderBy = DEFAULT_SORT_BY_ORDER_BY.MOVIES.ORDER_BY,
-    // filter
-    Year = "",
-  } = req.query || {};
-  const filterQuery = Year ? { Year } : {};
-  const findQuery = { ...filterQuery };
-  const sortQuery = [sortBy, orderBy === "desc" ? -1 : 1];
-  const total = await Movie.count(findQuery);
-  const movies = await Movie.find(findQuery)
-    .sort([sortQuery])
-    .skip(page - 1)
-    .limit(limit)
-    .exec();
-  res
-    .status(200)
-    .json({ data: movies, meta: { total, page, limit, sortBy, orderBy } });
+  try {
+    const {
+      page = DEFAULT_PAGINATION.PAGE,
+      limit = DEFAULT_PAGINATION.LIMIT,
+      sortBy = DEFAULT_SORT_BY_ORDER_BY.MOVIES.SORY_BY,
+      orderBy = DEFAULT_SORT_BY_ORDER_BY.MOVIES.ORDER_BY,
+      // filter
+      Year = "",
+    } = req.query || {};
+    const filterQuery = Year ? { Year } : {};
+    const findQuery = { ...filterQuery };
+    const sortQuery = [sortBy, orderBy === "desc" ? -1 : 1];
+    const total = await Movie.count(findQuery);
+    const movies = await Movie.find(findQuery)
+      .sort([sortQuery])
+      .skip(page - 1)
+      .limit(limit)
+      .exec();
+    res
+      .status(200)
+      .json({ data: movies, meta: { total, page, limit, sortBy, orderBy } });
+  } catch (err) {
+    res.status(500).json("Internal Server Error");
+  }
 });
 
 /* GET movie by Id */
 
 router.get("/:id", function (req, res, next) {
-  const id = req.params.id;
-  const movie = SAMPLE_MOVIES.find((movie) => movie.id === id);
-  if (movie) {
-    res.status(200).json(movie);
-  } else {
-    res.status(404).json("Resource Not Found");
+  try {
+    const id = req.params.id;
+    const movie = SAMPLE_MOVIES.find((movie) => movie.id === id);
+    if (movie) {
+      res.status(200).json(movie);
+    } else {
+      res.status(404).json("Resource Not Found");
+    }
+  } catch (error) {
+    res.status(500).json("Internal Server Error");
   }
 });
 
